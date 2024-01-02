920d809a000a96931e182a6ca7415a11b6897aec
kowsikadevi21082006 <deva36rp@gamil.com>
2024-01-02 21:07:35 +0530

    done

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const TodoItem = ({item : { data, isHidden } , index,dispatch}) => {
   return (
     <div style={{
-        backgroundColor : "purple",
+        backgroundColor : "red",
         width : "60vw" , 
-        filter : `${isHidden?"blur(1px)" : "blur(0px)"}`
+        filter : `${isHidden?"blur()" : "blur()"}`
     }}>
         <h3>{isHidden ? "This Content is Hidden" : data}</h3>
         <button onClick={()=>{
